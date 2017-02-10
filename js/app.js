b04981dae3bd31d8910e8d9137cc08057fc0b7e3
Ben Currie <benjamin.w.currie@gmail.com>
2017-02-10 16:36:29 +0000

    created function for policeman to appear at random times

--- js/app.js
+++ js/app.js
@@ -6,40 +6,51 @@ function setup() {
   const $right = $('#right');
   const $left = $('#left');
   const $will = $('.gArtist');
+  let randomTime = Math.ceil(Math.random()*8500);
+  console.log(randomTime);
 
 
   function appear(){
     $police.animate({ top: 0 });
   }
 
+  // Create a function that makes the image go back to it's previous position
+
+  function down(){
+    $police.animate({ bottom: 200 });
+  }
+
   function moveRight(){
     $will.animate({ left: '+=50px' }, 'slow');
   }
 
-  $left.click(function(){
+  function moveLeft(){
     $will.animate({ left: '-=50px' }, 'slow');
-  });
+  }
 
   function sprayWall() {
     // find the div covering the current wall section
     // update the opacity of that div
   }
 
-  // $right.click(function() {
-  //   $will.animate({ 'left': '+=50px' }, 'slow');
-  // });
-  //
-  // $left.click(function(){
-  //   $will.animate({ 'left': '-=50px' }, 'slow');
-  // });
+  function policeUp() {
+    setInterval(appear, randomTime);
+    policeDown();
+  }
+
+  //Function to make policeman go down is not working yet
+  function policeDown() {
+    setInterval(down, 10000);
+  }
+
+  policeUp();
 
 
   $checkCaught.on('click', appear);
   $right
     .on('mousedown', sprayWall)
     .on('mouseup', moveRight);
-
-
+  $left.on('click', moveLeft);
 }
 
 $(() => setup());
