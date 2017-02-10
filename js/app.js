d9541427c59aac099c8db880198a0843723547d6
Ben Currie <benjamin.w.currie@gmail.com>
2017-02-10 14:22:48 +0000

    created a moving div and the basic set up for the game

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
 console.log('JS loaded');
-
 function setup() {
 
+  const $police = $('.policemen');
+  const $checkCaught = $('.checkCaught');
+  const $right = $('#right');
+  const $left = $('#left');
+  const $will = $('.gArtist');
 
+
+  function appear(){
+    $police.slideUp(3000);
+  }
+
+  $right.click(function() {
+    $will.animate({ 'left': '+=50px' }, 'slow');
+  });
+
+  $left.click(function(){
+    $will.animate({ 'left': '-=50px' }, 'slow');
+  });
+
+
+  $checkCaught.on('click', appear);
 
 
 }
