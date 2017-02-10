d5f7dd9fc702a268b6300ba2e2885842797af984
Ben Currie <benjamin.w.currie@gmail.com>
2017-02-10 15:33:27 +0000

    made the policeman pop up at the click of a button

--- js/app.js
+++ js/app.js
@@ -9,19 +9,35 @@ function setup() {
 
 
   function appear(){
-    $police.slideUp(3000);
+    $police.animate({ top: 0 });
   }
 
-  $right.click(function() {
-    $will.animate({ 'left': '+=50px' }, 'slow');
-  });
+  function moveRight(){
+    $will.animate({ left: '+=50px' }, 'slow');
+  }
 
   $left.click(function(){
-    $will.animate({ 'left': '-=50px' }, 'slow');
+    $will.animate({ left: '-=50px' }, 'slow');
   });
+
+  function sprayWall() {
+    // find the div covering the current wall section
+    // update the opacity of that div
+  }
+
+  // $right.click(function() {
+  //   $will.animate({ 'left': '+=50px' }, 'slow');
+  // });
+  //
+  // $left.click(function(){
+  //   $will.animate({ 'left': '-=50px' }, 'slow');
+  // });
 
 
   $checkCaught.on('click', appear);
+  $right
+    .on('mousedown', sprayWall)
+    .on('mouseup', moveRight);
 
 
 }
