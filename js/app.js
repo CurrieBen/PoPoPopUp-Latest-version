4703bd8dd736a7f02240bda9e83ee953bea89de0
Ben Currie <benjamin.w.currie@gmail.com>
2017-02-12 13:36:40 +0000

    created a random speed at which policeman appears and added basic images

--- js/app.js
+++ js/app.js
@@ -2,22 +2,22 @@ console.log('JS loaded');
 function setup() {
 
   const $police = $('.policemen');
-  const $checkCaught = $('.checkCaught');
   const $right = $('#right');
   const $left = $('#left');
   const $will = $('.gArtist');
-  let randomTime = Math.ceil(Math.random()*8500);
+  const randomTime = Math.ceil(Math.random()*8500);
+  const randomTimeUp = Math.ceil(Math.random()*3000);
+  const randomTimeDown = Math.ceil(Math.random()*10000);
   console.log(randomTime);
 
 
   function appear(){
-    $police.animate({ top: 0 });
+    $police.animate({ top: 0 }, randomTimeUp, down);
   }
 
   // Create a function that makes the image go back to it's previous position
-
   function down(){
-    $police.animate({ bottom: 200 });
+    $police.animate({ bottom: 200 }, randomTimeDown);
   }
 
   function moveRight(){
@@ -35,20 +35,17 @@ function setup() {
 
   function policeUp() {
     setInterval(appear, randomTime);
-    policeDown();
   }
 
   //Function to make policeman go down is not working yet
-  function policeDown() {
-    setInterval(down, 10000);
-  }
-
-  policeUp();
+  // function policeDown() {
+  //   setInterval(down, 10000);
+  // }
 
 
-  $checkCaught.on('click', appear);
   $right
     .on('mousedown', sprayWall)
+    .on('click', policeUp)
     .on('mouseup', moveRight);
   $left.on('click', moveLeft);
 }
