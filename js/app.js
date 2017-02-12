a468c403329b6802efca7b1c393819ddf1854c3e
Ben Currie <benjamin.w.currie@gmail.com>
2017-02-12 16:12:14 +0000

    edited the opacity changer so it keeps changing while mouse down rather than on mouse click

--- js/app.js
+++ js/app.js
@@ -7,18 +7,19 @@ function setup() {
   const $will = $('.gArtist');
   const $section = $('.graff');
   let i = 0.1;
-  let x = 0.1;
+  const x = 0.015;
   const randomTime = Math.ceil(Math.random()*8500);
   const randomTimeUp = Math.ceil(Math.random()*3000);
   const randomTimeDown = Math.ceil(Math.random()*10000);
   console.log(randomTime);
+  console.log(randomTimeDown);
 
 
   function appear(){
-    $police.animate({ top: 0 }, randomTimeUp, down);
+    $police.animate({ top: 0 }, randomTimeUp, down());
   }
 
-  // Create a function that makes the image go back to it's previous position
+  // Create a function that makes the image go back to it's previous position. Below does not work - need to work on this
   function down(){
     $police.animate({ bottom: 200 }, randomTimeDown);
   }
@@ -31,12 +32,23 @@ function setup() {
     $will.animate({ left: '-=53px' }, 'slow');
   }
 
+  var slowly;
+
+  $('#right').mousedown(function(){
+    slowly = setInterval(function() {
+      sprayWall();
+    }, 100);
+  }).mouseup(function() {
+    clearInterval(slowly);
+  });
+
   function sprayWall() {
     // find the div covering the current wall section
     // update the opacity of that div
     $section.css('opacity', i);
     i = i+x;
   }
+
 
   function policeUp() {
     setInterval(appear, randomTime);
