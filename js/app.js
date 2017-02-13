18f02ce6116470894064bfcaad63b4617a26f68b
Ben Currie <benjamin.w.currie@gmail.com>
2017-02-13 14:33:22 +0000

    created check to see if you get caught and also made policeman go up and down on a loop

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 console.log('JS loaded');
+
 function setup() {
 
   const $police = $('.policemen');
@@ -6,8 +7,12 @@ function setup() {
   const $left = $('#left');
   const $will = $('.gArtist');
   const $section = $('.graff');
-  let i = 0.1;
-  const x = 0.015;
+  // const $everything = $('*');
+  let currentSection = 0;
+  // let i = 0.1;
+  const x = 0.03;
+  let mousedown = false;
+  let policeInSight = false;
   const randomTime = Math.ceil(Math.random()*8500);
   const randomTimeUp = Math.ceil(Math.random()*3000);
   const randomTimeDown = Math.ceil(Math.random()*10000);
@@ -17,48 +22,80 @@ function setup() {
 
   function appear(){
     // $police.animate({ top: 0 }, randomTimeUp, down()); having down at the end is the complete part and it means that it will run this function once the rest has completed
-    $police.animate({ top: 0 }, randomTimeUp);
+    console.log('inside appear()');
+    $police.animate({ top: 0 }, randomTimeUp, disappear);
   }
 
   // Create a function that makes the image go back to it's previous position. Below does not work - need to work on this
-  // function down(){
-  //   $police.animate({ bottom: 200 }, randomTimeDown);
-  // }
+  function disappear(){
+    console.log('inside disappear()');
+    policeInSight = true;
+    console.log(policeInSight);
+    setTimeout(() => {
+      $police.animate({ top: 140 }, randomTimeDown, policeUp);
+    }, randomTime);
+  }
 
   function moveRight(){
     $will.animate({ left: '+=53px' }, 'slow');
+    currentSection++;
+    console.log(currentSection);
   }
 
   function moveLeft(){
     $will.animate({ left: '-=53px' }, 'slow');
+    currentSection--;
+    console.log(currentSection);
   }
 
   var slowly;
 
   $('#right').mousedown(function(){
     slowly = setInterval(function() {
+      mousedown = true;
       sprayWall();
     }, 100);
   }).mouseup(function() {
     clearInterval(slowly);
+    mousedown = false;
   });
 
   function sprayWall() {
     // find the div covering the current wall section
     // update the opacity of that div
-    $section.css('opacity', i);
-    i = i+x;
+    const currentOpacity = parseFloat($section.eq(currentSection).css('opacity'));
+    const newOpacity = currentOpacity + x;
+    $section.eq(currentSection).css('opacity', newOpacity);
+
+    checkCaught();
   }
 
-
   function policeUp() {
-    setInterval(appear, randomTime);
+    policeInSight = false;
+    console.log('inside policeUp()');
+    // setInterval(appear, randomTime);
+    // setTimeout( function() {
+    //   policeInSight = true;
+    // }, randomTime+randomTimeUp);
+    // setInterval(disappear, randomTime+randomTimeUp);
+    setTimeout(() => {
+      appear();
+    }, randomTime);
   }
 
   //Function to make policeman go down is not working yet
-  // function policeDown() {
-  //   setInterval(down, 10000);
+  //  function policeDown() {
+  //  setInterval(down, 10000);
+  //  policeInSight = false;
   // }
+
+  function checkCaught() {
+    if (mousedown === true && policeInSight === true){
+      //change the image below to something better when you get caught
+      // $everything.css('background-image', 'url(/Users/123THC/development/project1/images/joke.jpg)');
+      console.log('You\'re nicked guv!');
+    }
+  }
 
 
   $right
