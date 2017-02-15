ecda84a89d50ff876f6a4984f5598f63431997de
Ben Currie <benjamin.w.currie@gmail.com>
2017-02-15 17:05:39 +0000

    fixed the rest button so it works, and sorted the win page - just need to do lose page now

--- js/app.js
+++ js/app.js
@@ -16,9 +16,12 @@ function setup() {
   const $warning = $('.warning');
   const $timer = $('.time');
   const $shake = $('.shake');
-  // const $winning = $('.winning');
-  const $bottomSection = $('.behind');
-  // const $tryAgain = $('.tryAgain');
+  const $winning = $('.winning');
+  const $menu = $('.menu');
+  // const $winningBackground = $('.winningBackground');
+  const $tryAgain = $('.tryAgain');
+  const $howToGuide = $('.howToGuide');
+  const $info = $('.howToPlay');
   let currentSection = 0;
   const x = 0.03;
   let mousedown = false;
@@ -30,10 +33,20 @@ function setup() {
   let newOpacity = null;
   let score = 0;
   let warnings = 0;
-  let timeRemaining = 120;
+  let timeRemaining = 60;
+  let intervalId = null;
+  let blinkerInterval = null;
+  let slowly = null;
 
   $begin.on('click' , ()=>{
     firstSound();
+    startTime();
+    setTimeout(() => {
+      policeUp();
+    }, 2000);
+    setTimeout(() => {
+      $menu.removeClass('intro');
+    }, 2000);
     $('html, body').animate({ scrollTop: $(document).height() }, 2000, () => {
       $('html').addClass('locked');
     });
@@ -41,14 +54,20 @@ function setup() {
   });
 
   function startTime() {
-    // showInput();
-    setInterval(() => {
+    intervalId = setInterval(() => {
       if (timeRemaining > 0) {
         timeRemaining--;
         checkWin();
         $timer.html('Time Remaining: '+timeRemaining);
       }
     }, 1000);
+  }
+
+  function howToPlay() {
+    $howToGuide.css('visibility', 'visible');
+    setInterval(() => {
+      $howToGuide.css('visibility', 'hidden');
+    }, 15000);
   }
 
   function firstSound() {
@@ -61,13 +80,13 @@ function setup() {
 
   function appear(){
     console.log('inside appear()');
-    $police.animate({ top: 790 }, timeUp, disappear);
+    $police.animate({ top: 775 }, timeUp, disappear);
   }
 
   function disappear(){
     policeInSight = true;
     setTimeout(() => {
-      $police.animate({ top: 880 }, timeDown, policeUp);
+      $police.animate({ top: 867 }, timeDown, policeUp);
     }, randomTime);
   }
 
@@ -86,8 +105,6 @@ function setup() {
   function turnAround() {
     $artist.css('transform', 'scaleX(1)');
   }
-
-  let slowly = 0;
 
   $('#right').mousedown(function(){
     slowly = setInterval(function() {
@@ -108,7 +125,7 @@ function setup() {
     if (parseFloat(newOpacity)<1){
       score++;
     }
-    $scoreboard.html('Score: ' + score);
+    $scoreboard.html('Completed: ' + Math.round(score/5.28) + ' %');
     checkCaught();
   }
 
@@ -128,7 +145,7 @@ function setup() {
       $warning.css('color', '#f00');
     }
     if (warnings === 2){
-      setInterval(blinker, 500);
+      blinkerInterval = setInterval(blinker, 500);
     }
     if (warnings === 3){
       //some code to stop everything and offer reset button
@@ -143,25 +160,47 @@ function setup() {
 
   function checkWin() {
     if(timeRemaining === 0 && warnings < 3 ){
+      // $menu.css('visibility', 'hidden');
+      // $winningBackground.css('visibility', 'visible');
+      $winning.html('Sick One Blud! You got '+ Math.round(score/5.28) + '% of the painting done!');
       // $winning.css('visibility', 'visible');
-      // $winning.html('Sick One Blud! You got '+ Math.round(score/3.96) + '% of the painting done!');
-      $bottomSection.css('visibility', 'hidden');
+      // $tryAgain.css('visibility', 'visible');
+      $(window).scrollTop(0);
+      // console.log('working');
     }
   }
 
-  // function reset() {
-  //
-  // }
+  function reset() {
+    clearInterval(intervalId);
+    policeInSight = false;
+    mousedown = false;
+    $menu.addClass('intro');
+    $winning.html('');
+    $scoreboard.html('Completed: 0%');
+    $warning.html('Warnings: 0');
+    $warning.css('color', '#000');
+    $section.css('opacity', '0');
+    $artist.css('left', 'auto');
+    clearInterval(blinkerInterval);
+    currentOpacity = null;
+    newOpacity = null;
+    score = 0;
+    warnings = 0;
+    timeRemaining = 120;
+    currentSection = 0;
+    $timer.html('Time Remaining: '+timeRemaining);
+    $(window).scrollTop(0);
+  }
 
 
   $right
     .on('mousedown', sprayWall)
-    .on('click', policeUp)
-    .on('mousedown', startTime)
     .on('mouseup', moveRight);
   $left
     .on('mousedown', moveLeft)
     .on('mouseup', turnAround);
+  $tryAgain.on('click', reset);
+  $info.on('click', howToPlay);
 }
 
 $(() => setup());
